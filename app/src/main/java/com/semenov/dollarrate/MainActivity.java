2e4fe3bb3830768bd055c6ba39b7d123c94098d1
Nikolui333 <semenovnikolu@yandex.ru>
2020-08-08 10:21:36 +0300

    the list is now populated with data

--- app/src/main/java/com/semenov/dollarrate/MainActivity.java
+++ app/src/main/java/com/semenov/dollarrate/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
-    private void init()
+/*    private void init()
     {
         runnable = new Runnable() {
             @Override
@@ -51,6 +51,38 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter adapter = new MyAdapter(arrayList);
         adapter.update(arrayList);
         recyclerView.setAdapter(adapter);
+    }*/
+
+    private void init() {
+        // инициализируем всё что нужно
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        arrayList = new ArrayList<>();
+        final MyAdapter adapter = new MyAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
+
+        // запускаем задачу загрузки
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                // getWeb();
+                refresh();
+                // после загрузки уведомляем адаптер что данные изменились
+                // делать это можно только в UI-потоке - поэтому
+                // кидаем вызов в UI-поток любым способом
+                recyclerView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+
+                });
+            }
+/*        });
+    }*/
+};
+    secThread = new Thread(runnable);
+            secThread.start();
     }
 
     public void refresh(){
