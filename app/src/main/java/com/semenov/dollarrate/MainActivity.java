36abe0399a31cb9a83a4d889ef7fd3bcc54fa22b
Nikolui333 <semenovnikolu@yandex.ru>
2020-08-08 10:42:37 +0300

    commit

--- app/src/main/java/com/semenov/dollarrate/MainActivity.java
+++ app/src/main/java/com/semenov/dollarrate/MainActivity.java
@@ -24,36 +24,16 @@ public class MainActivity extends AppCompatActivity {
     private Document doc;
     private Thread secThread; // второстепенный поток
     private Runnable runnable;
-    private ListView listView;
     private ArrayList<ListItemClass> arrayList = new ArrayList<ListItemClass>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        launch();
     }
 
-/*    private void init()
-    {
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-               // getWeb();
-                refresh();
-            }
-        };
-        secThread = new Thread(runnable);
-        secThread.start();
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        MyAdapter adapter = new MyAdapter(arrayList);
-        adapter.update(arrayList);
-        recyclerView.setAdapter(adapter);
-    }*/
-
-    private void init() {
+    private void launch() {
         // инициализируем всё что нужно
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -78,8 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
                 });
             }
-/*        });
-    }*/
 };
     secThread = new Thread(runnable);
             secThread.start();
@@ -88,25 +66,17 @@ public class MainActivity extends AppCompatActivity {
     public void refresh(){
         try {
                 doc = Jsoup.connect("https://yandex.ru/news/quotes/1.html").get();
-                Elements tables = doc.getElementsByTag("tbody");
-                Element our_table = tables.get(0);
-                Elements elements_from_table = our_table.children();
-                Element dollar = elements_from_table.get(0);
+                Elements data = doc.getElementsByTag("tbody");
+                Element piece_of_data = data.get(0);
+                Elements elements_piece_of_data = piece_of_data.children();
+                Element dollar = elements_piece_of_data.get(0);
                 Elements dollar_elements = dollar.children();
-                Log.d("MyLog","Tbody size : " + our_table.children().get(0).text());
-                for(int i = 0;i < our_table.childrenSize();i++ )
+                Log.d("MyLog","Tbody size : " + piece_of_data.children().get(0).text());
+                for(int i = 0;i < piece_of_data.childrenSize();i++ )
                 {
-/*                    arrayList.add(new ListItemClass(our_table.children().get(i).child(0).text(),
-                            our_table.children().get(i).child(1).text(),
-                            our_table.children().get(i).child(2).text()));*/
-
-                    ListItemClass items = new ListItemClass();
-                    items.setData_1(our_table.children().get(i).child(0).text());
-                    items.setData_2(our_table.children().get(i).child(1).text());
-                    items.setData_3(our_table.children().get(i).child(2).text());
-                    arrayList.add(items);
-                   // Log.d("OneLog", list.get(0));
-                    Log.d("OneLog", String.valueOf(arrayList.get(0)));
+                    arrayList.add(new ListItemClass(piece_of_data.children().get(i).child(0).text(),
+                            piece_of_data.children().get(i).child(1).text(),
+                            piece_of_data.children().get(i).child(2).text()));
                 }
 
         } catch (Exception ex){
